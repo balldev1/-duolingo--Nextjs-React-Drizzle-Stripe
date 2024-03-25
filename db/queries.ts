73e58672515d9460a6db38ac93ae37031b1252a2
balldev1 <balldev1@hotmail.com>
2024-03-26 05:08:34 +0700

    ีีีีUserProgress 2.50

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,36 @@
 import { cache } from 'react';
 import db from '@/db/drizzle';
+import {auth} from "@clerk/nextjs";
+import {eq} from "drizzle-orm";
+import {courses, userProgress} from "@/db/schema";
 
+// get user
+export const getUserProgress = cache(async()=>{
+    const { userId } = await auth();
+
+    if (!userId){
+        return null;
+    }
+
+    const data = await db.query.userProgress.findFirst({
+        where: eq(userProgress.userId,userId),
+        with:{
+            activeCourse: true,
+        },
+    });
+})
+
+// get courses
 export const getCourses = cache(async () => {
     const data = await db.query.courses.findMany();
     return data;
 });
+
+// get courses.id
+export const getCoursesById = cache(async(courseId:number)=>{
+    const data = await db.query.courses.findFirst({
+        where:eq(courses.id, courseId),
+        // TODO
+    });
+    return data;
+})
--- db/schema.ts
+++ db/schema.ts
@@ -1,11 +1,19 @@
-import {integer, pgTable, serial, text} from 'drizzle-orm/pg-core';
+import {integer, pgTable,  serial, text} from 'drizzle-orm/pg-core';
+import {relations} from "drizzle-orm";
 
+// courses
 export const courses = pgTable("courses",{
     id: serial("id").primaryKey(),
     title: text("title").notNull(),
     imageSrc: text("image_src").notNull(),
 });
 
+// ความสัมพันธ์ระหว่างตารางตัวเอง
+export const coursesRelations = relations(courses,({many})=>({
+    userProgress: many(userProgress),
+}))
+
+// user
 export const userProgress = pgTable("user_progress", {
     userId: text("user_id").primaryKey(),
     userName: text("user_name").notNull().default("User"),
@@ -14,3 +22,11 @@ export const userProgress = pgTable("user_progress", {
     hearts: integer("hearts").notNull().default(5),
     points: integer("points").notNull().default(0),
 });
+
+// ความสัมพันธ์ระหว่างตารางuserProgress ,กับตารางอื่น
+export const userProgressRelations = relations(userProgress,({one})=>({
+    activeCourse: one(courses, {
+        fields: [userProgress.activeCourseId],
+        references: [courses.id],
+    }),
+}));
