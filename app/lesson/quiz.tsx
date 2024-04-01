b3a8841ac5bc03b2bf64bb6dd032b285ff209ebf
balldev1 <balldev1@hotmail.com>
2024-04-01 15:59:39 +0700

    yt 6.0

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -3,6 +3,8 @@
 import {challengeOptions, challenges} from "@/db/schema";
 import {useState} from "react";
 import {Header} from "@/app/lesson/header";
+import {QuestionBubble} from "@/app/lesson/question-bubble";
+import {Challenge} from "@/app/lesson/challenge";
 
 type Props = {
     initialPercentage : number;
@@ -25,6 +27,20 @@ export const Quiz = ({
 
     const [hearts, setHearts] = useState( initialHearts);
     const [percentage, setPercentage] = useState(initialPercentage);
+    const [challenges] = useState(initialLessonChallenges);
+    // ให้ activeIndex === Index.challenges , !challenge.completed
+    const [activeIndex, setActiveIndex] = useState(()=>{
+        const uncompletedIndex = challenges.findIndex((challenge)=>!challenge.completed);
+        //ถ้าได้ต่ำกว่า -1 ให้แสดงเป็น 0 / ถ้าได้มากกว่า 0 ให้แสดงเลข uncompletedIndex
+        return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+    });
+
+    const challenge = challenges[activeIndex];
+    const options = challenge?.challengeOptions ?? [];
+
+    const title = challenge.type === "ASSIST"
+    ? "Select the correct meaning"
+        : challenge.question
 
     return (
         <>
@@ -33,6 +49,33 @@ export const Quiz = ({
             percentage={percentage}
             hasActiveSubscription={!!userSubscription?.isActive}
             />
+            <div className="flex-1">
+                <div className="h-full flex items-center justify-center">
+                    <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0
+                    flex flex-col gap-y-12
+                    ">
+                        <h1 className="text-lg lg:text-3xl text-center lg:text-start
+                        font-bold text-neutral-700">
+                            {title}
+                        </h1>
+                        <div>
+                        { challenge.type === "ASSIST" && (
+                            <QuestionBubble
+                            question={challenge.question}
+                            />
+                        ) }
+                            <Challenge
+                                options={options}
+                                onSelect={()=>{}}
+                                status="none"
+                                selectedOption={undefined}
+                                disabled={false}
+                                type={challenge.type}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
